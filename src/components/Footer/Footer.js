f0004c8b6ef733bf0fe585bd8d4af767a46a7a7c
rahid <hidayat.85@gmail.com>
2017-12-08 14:46:15 +0700

    template header and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// stateless component
+const Footer = () => {
+  return (
+    <div className="Footer">
+      My Footer
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        My HEader
+      </div>
+    );
+  }
+}
+
+export default Header;
