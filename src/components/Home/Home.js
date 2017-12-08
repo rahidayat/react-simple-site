0c896e9a6edbdd6536d1f62e15acac7b2c9bbcc6
rahid <hidayat.85@gmail.com>
2017-12-08 18:32:14 +0700

    first section at home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import './Home.css'
 
 
 class Home extends Component {
   render() {
+    let heading = "Knowledge is the light"
+    let subtitle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     return (
-      <div className="App">
-        My HOME
+      <div>
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">{ heading }</h1>
+              <div className="is-two-thirds column is-padingless">
+                <h2 className="subtitle is-4">{ subtitle }</h2>
+              </div>
+              <a className="button is-large is-primary" id="learn">Learn More</a>
+            </div>
+          </div>
+        </section>
+
       </div>
     );
   }
