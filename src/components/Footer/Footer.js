123f5f51a79d86f22815c3ddd81d257d88cf7fe5
rahid <hidayat.85@gmail.com>
2017-12-08 17:20:39 +0700

    footer styling and fixing toggle menu while responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Footer.css'
 
 // stateless component
 const Footer = () => {
   return (
-    <div className="Footer">
-      My Footer
+    <div>
+      <footer className="footer is-primary">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <p>Footer is right here Footer is right hereFooter is right hereFooter is right hereFooter is right hereFooter is right hereFooter is right here</p>
+            </div>
+            <div className="column has-text-right">
+              <a className="icon"><i className="fa fa-facebook"></i></a>
+              <a className="icon"><i className="fa fa-twitter"></i></a>
+
+            </div>
+          </div>
+
+        </div>
+
+      </footer>
     </div>
   );
 }
